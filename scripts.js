d7403d3787cad99791296ccff6d92b092df877e1
ademilsonpires <89095980+ademilsonpires@users.noreply.github.com>
2022-12-03 22:04:06 -0300

    Passagen de valores dinamicamente para o HTML e formatação de valores como moeda pt-BR via javascript - Start - scripts.js v1.4

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const Modal = {
-    open(){
+    open() {
         //abrir Modal
         //Adicionar a class active ao modal para abrir
         document.querySelector('.modal-overlay')
@@ -7,7 +7,7 @@ const Modal = {
 
 
     },
-    close(){
+    close() {
         //fechar o modal
         //remover a classe active no modal
         document
@@ -21,3 +21,106 @@ const Modal = {
 
 }
 
+const transactions = [{
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+},
+{
+    id: 2,
+    description: 'Website',
+    amount: 50000,
+    date: '23/01/2021'
+},
+{
+    id: 3,
+    description: 'Internet',
+    amount: -20001,
+    date: '23/01/2021'
+},
+{
+    id: 4,
+    description: 'Aluguel',
+    amount: -150000,
+    date: '23/01/2021'
+},
+{
+    id: 5,
+    description: 'Salário',
+    amount: 50000,
+    date: '01/01/2021'
+},
+
+]
+
+const Transaction = {
+    incomes() {
+        //somar entradas 
+    },
+    expenses() {
+        //somar saídas
+
+    },
+
+    totals() {
+        //somar totais
+
+    }
+
+}
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    
+    addTransaction(transaction, index){
+       
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+       
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = `
+           <td class="description">${transaction.description}</td>
+           <td class="${CSSclass}">${amount}</td>
+           <td class="date">${transaction.date}</td>
+           <td>
+                <img src="./assets/minus.svg" alt="Remover Transação">
+           </td>
+          `
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? '-' : ''
+        value = String(value).replace(/\D/g, "")
+        value = Number(value)/100
+        value = value.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+
+
+        })
+
+
+
+       return signal + value
+
+    }
+
+
+
+
+}
+
+transactions.forEach(function (transaction){
+    DOM.addTransaction(transaction)
+
+})
